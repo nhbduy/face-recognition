56394b4021ed5a2821bf67586c756d3c720316ba
Duy Nguyen <duy.nguyen@christinas.vn>
2019-10-10 17:26:35 +0700

    Updates

---
+++ precache-manifest.01d7b75f593fb2d0327c1a570905be89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dfaaa22298845f53f00c686f25a0d95",
+    "url": "/face-recoginiton/index.html"
+  },
+  {
+    "revision": "d440bed97661ec9470ec",
+    "url": "/face-recoginiton/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "72a8c70c3f63fc1a62b7",
+    "url": "/face-recoginiton/static/css/main.9e8eb87f.chunk.css"
+  },
+  {
+    "revision": "d440bed97661ec9470ec",
+    "url": "/face-recoginiton/static/js/2.ca72daa2.chunk.js"
+  },
+  {
+    "revision": "72a8c70c3f63fc1a62b7",
+    "url": "/face-recoginiton/static/js/main.6ec39e56.chunk.js"
+  },
+  {
+    "revision": "f913783a0ad89fbbeadb",
+    "url": "/face-recoginiton/static/js/runtime-main.ded2d98b.js"
+  }
+]);
