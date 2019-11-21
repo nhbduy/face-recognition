9861228228c85443247d2b8773ad52a2635499bd
Duy Nguyen <duy.nguyen@christinas.vn>
2019-11-21 21:57:48 +0700

    Updates

--- precache-manifest.08f314165021a6036ec809af0e26882b.js
+++ precache-manifest.08f314165021a6036ec809af0e26882b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "344a8086074690b343d560cf853b1f46",
+    "revision": "633ca878d74756feb12988cbb66ea6e9",
     "url": "/face-recognition/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/face-recognition/static/css/2.eb45ad44.chunk.css"
   },
   {
-    "revision": "442d1f53627dc03271aa",
+    "revision": "fc3a4e9626187c0c7473",
     "url": "/face-recognition/static/css/main.93942273.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/face-recognition/static/js/2.c5d7391c.chunk.js"
   },
   {
-    "revision": "442d1f53627dc03271aa",
-    "url": "/face-recognition/static/js/main.3051af22.chunk.js"
+    "revision": "fc3a4e9626187c0c7473",
+    "url": "/face-recognition/static/js/main.c90e07a1.chunk.js"
   },
   {
     "revision": "e22fbd4e5ba5d32fe548",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/face-recognition/precache-manifest.2678229f58a8a0b11558499db5093783.js"
+  "/face-recognition/precache-manifest.08f314165021a6036ec809af0e26882b.js"
 );
 
 self.addEventListener('message', (event) => {
