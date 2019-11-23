28cd2e17c83bcd7a1aa9fdc6373037b58e912866
Duy Nguyen <duy.nguyen@christinas.vn>
2019-11-23 10:55:42 +0700

    Changes config file

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ const SERVER_URL_DEV = 'http://localhost:5000';
 const SERVER_URL_PROD = 'https://nhbduy-face-recognition-server.herokuapp.com';
 
 export default {
-  SERVER_URL: MODE === 1 ? SERVER_URL_DEV : SERVER_URL_PROD
+  SERVER_URL: MODE === 2 ? SERVER_URL_PROD : SERVER_URL_DEV
 };
